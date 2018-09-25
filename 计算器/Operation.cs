d1ca53a98dd586e176a747adb5c17561ee490cd4
赵坤 <zk9474@hotmail.com>
2018-09-25 22:20:06 +0800

    添加项目文件。

---
+++ 计算器/Operation.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 计算器
+{
+    public abstract class Operation 
+    {
+        public double NumberA { get; set; }
+        public double NumberB { get; set; }
+
+        public abstract double GetResult();
+    }
+
+    public class OperationAdd : Operation
+    {
+        public override double GetResult()
+        {
+            return NumberA + NumberB;
+        }
+    }
+
+    public class OperationSub : Operation
+    {
+        public override double GetResult()
+        {
+            return NumberA - NumberB;
+        }
+    }
+
+    public class OperationMul : Operation
+    {
+        public override double GetResult()
+        {
+            return NumberA * NumberB;
+        }
+    }
+
+    public class OperationDiv : Operation
+    {
+        public override double GetResult()
+        {
+            return NumberA / NumberB;
+        }
+    }
+
+    public sealed class OperationFactory
+    {
+        public static Operation Create(string operat)
+        {
+            Operation operation = null;
+            switch (operat)
+            {
+                case "+":
+                    operation = new OperationAdd();
+                    break;
+                case "-":
+                    operation = new OperationSub();
+                    break;
+                case "*":
+                    operation = new OperationMul();
+                    break;
+                case "/":
+                    operation = new OperationDiv();
+                    break;
+                default:
+                    throw new InvalidOperationException("指定操作符的运算没有实现，操作符：" + operat);
+            }
+
+            return operation;
+        }
+    }
+}
---
+++ 计算器/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace 计算器
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("请输入运算符（+ - * / ）：");
+                var operat = Console.ReadLine();
+                var operation = OperationFactory.Create(operat);
+
+                Console.Write("请输入NumberA：");
+                operation.NumberA = double.Parse(Console.ReadLine());
+                Console.Write("请输入NumberB：");
+                operation.NumberB = double.Parse(Console.ReadLine());
+                Console.WriteLine("结果：" + operation.GetResult());
+
+                Console.WriteLine("按任意键进入下一次运算，按 q 退出。");
+                var r = Console.ReadLine();
+                if (r != null && r.Equals("q"))
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
